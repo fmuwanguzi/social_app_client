0304d4696598d51285f02d81d1d3488099608307
Felix Muwanguzi <72531389+fmuwanguzi@users.noreply.github.com>
2021-01-20 09:28:29 -0500

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <footer className="footer bg-dark">
+            <div className="container text-center">
+                <span className="text-muted">
+                    @2021 built by Felix M (during COVID) 
+                </span>
+            </div>
+        </footer>    
+    )
+}
+
+export default Footer;
