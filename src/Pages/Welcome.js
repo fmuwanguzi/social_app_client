6ccb5bbb982f815fa5bf19a992b27591cafcf329
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2021-01-20 22:09:00 -0800

    [Welcome.js] Finish routing landing page

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Welcome = () => {
   return (
@@ -6,12 +7,17 @@ const Welcome = () => {
       <h1>KickIt</h1>
       <p>Connect with friends, family and many others around the world.</p>
       <div className="btn-container">
-        <button type="button" className="login-btn">
-          Log In
-        </button>
-        <button type="button" class="signup-btn">
-          Sign Out
-        </button>
+        <Link to='/login'>
+          <button type="button" className="login-btn">
+            Log In
+          </button>
+        </Link>
+
+        <Link to='signup'>
+          <button type="button" class="signup-btn">
+            Sign Out
+          </button>
+        </Link>
       </div>
     </div>
   );
